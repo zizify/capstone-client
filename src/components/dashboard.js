e488a920397079d49aa81cef87d646ebf3c43aa6
Onesun15 <sunnysharma15@hotmail.com>
2018-01-16 19:14:53 -0500

    student data renders on student dashboard after login

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -11,9 +11,9 @@ export class Dashboard extends React.Component {
   }
 
   render() {
-    if (!this.props) {
-      return <h1>Loading....</h1>;
-    }
+    // if (!this.props) {
+    //   return <h1>Loading....</h1>;
+    // }
     
     return (
       <div className="dashboard">
