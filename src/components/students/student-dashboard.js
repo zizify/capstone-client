5e42196e8238ba76c0cdc0b5c165742543bbf39e
Onesun15 <sunnysharma15@hotmail.com>
2018-01-16 20:06:59 -0500

    test for render of student data

--- src/components/students/student-dashboard.js
+++ src/components/students/student-dashboard.js
@@ -14,14 +14,17 @@ export class StudentDashboard extends React.Component {
       console.log('InLoadingMode Student', this.props.student)
       return <h1>Loading....</h1>;
    }
+    let studentClassName = null;
     const studentData = this.props.student.relevant.map((student, index) => {
+      studentClassName = student.className;
       return <li key={index}> {student.className}, {student.subject}, {student.title}, {student.dueDate}, {student.goals}, {student.points}, {student.teacher}, {student.instructions}</li>;
     });
-      console.log('Mapped_STUDENTDATA', studentData);
+      console.log('Mapped_STUDENTDATA', studentData[0]);
     return (
       <div className="student-dashboard">
         <h1 className="student-greeting"> Hello, {this.props.name} </h1>
         <ul>{studentData}</ul>
+        {studentClassName}
       </div>
     );
   }
