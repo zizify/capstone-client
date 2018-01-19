3db471e09a7a5df2bda0c456d95b423feb355d47
Onesun15 <sunnysharma15@hotmail.com>
2018-01-19 12:14:37 -0500

    redirect based on isTeacher

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,7 @@ import {Route, withRouter} from 'react-router-dom';
 import HeaderBar from './header-bar';
 import LandingPage from './landing-page';
 import Dashboard from './dashboard';
+import TeacherDashboard from './teachers/teacher-dashboard';
 import RegistrationPage from './registration-page';
 import {refreshAuthToken} from '../actions/auth';
 
@@ -44,6 +45,7 @@ export class App extends React.Component {
                 <HeaderBar />
                 <Route exact path="/" component={LandingPage} />
                 <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/dashboard/teacher" component={TeacherDashboard} />
                 <Route exact path="/register" component={RegistrationPage} />
             </div>
         );
