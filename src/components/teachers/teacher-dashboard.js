22f4e8b203db2027beab41c635e52dcb6e18773b
Jesse <dev@jesseparrott.com>
2018-01-23 16:50:35 -0600

    Fixes merge conflicts conflicts

--- src/components/teachers/teacher-dashboard.js
+++ src/components/teachers/teacher-dashboard.js
@@ -97,20 +97,19 @@ export class TeacherDashboard extends React.Component {
           }}
         />
         <ul>{teacherData()}</ul>
-
-    return (  
-      <div className="dashboard">
+        <div className="dashboard">
             <HeaderNav />
         <div className="dashboard-username">
           Username: {this.props.username}
         </div>
         <h1 className="teacher-greeting"> Hi, {this.props.name} </h1>
         <h3 className="teacher-assignments">You may create assignments for your class here</h3>
-
       </div>
-    );
+</div>
+    )
   }
 }
+
 
 const mapStateToProps = state => {
   console.log('TeacherDashboard', state);
