72e6b353b3c760622b37a0cf69afc2262674d6c5
Jesse <dev@jesseparrott.com>
2018-01-23 18:06:44 -0600

    fixes warning in dashboard

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {clearAuth} from '../actions/auth';
-import {clearAuthToken} from '../local-storage';
 import { HeaderNav } from './header-nav'
 import { HeaderHero } from './header-hero';
 
 export class Header extends React.Component {
-    logOut() {
-        this.props.dispatch(clearAuth());
-        clearAuthToken();
-    }
-
     render() {
-        // Only render the log out button if we are logged in
-        let logOutButton;
-        if (this.props.loggedIn) {
-            logOutButton = (
-                <button onClick={() => this.logOut()}>Log out</button>
-            );
-        }
         return (
             <div className="header">
             <HeaderNav />
