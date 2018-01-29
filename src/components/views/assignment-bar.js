21c0660aae06f48e36333fc56d8c10f9dfd10d18
Jesse <dev@jesseparrott.com>
2018-01-29 10:16:58 -0600

    Adds styles to user assignments

--- src/components/views/assignment-bar.js
+++ src/components/views/assignment-bar.js
@@ -1,27 +1,25 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
+
 export default function AssignmentBar(props){
   let bars = props.bars;
   const mappedBars = bars.map((each, index) => {
     return (
-      <div 
-        className="assignment"
+      <Collapsible
+        trigger={`${each.className}${each.title}${each.dueDate.date}`}
+        className="student-assignment-card"
         key={index}
         >
-      <div className="assignment__header">
-        <h3>{each.className}</h3>
-        <h3>{each.title}</h3>
-        <h3>{each.dueDate.date}</h3>
-      </div>
       <div className="assignment__details">
         <h3>{each.subject}</h3>
         <h3>Goals</h3>
           <h4>{each.goals}</h4>
         <h3>{each.comments}</h3>
       </div>
-    </div>
+    </Collapsible>
     )
   })
   return (
-    <h1>{mappedBars}</h1>
+    <div>{mappedBars}</div>
   )
   }
