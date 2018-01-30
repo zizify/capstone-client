b1cae3c926e212e9e90cae44d6ed86eae2e09132
Jesse <dev@jesseparrott.com>
2018-01-30 12:11:05 -0600

    adds a teacher assignment form

--- src/components/views/teacher-assignment-form.js
+++ src/components/views/teacher-assignment-form.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-export default function TeacherAssignmentForm(props) {
+export default class TeacherAssignmentForm extends React.Component {
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.name.value, e.target.usernames.value);
+    const userIds = e.target.usernames.value.split(', ')
+    const authToken = localStorage.authToken;
+    fetch('http://localhost:8080/api/users/class/create', {
+      method: 'POST',
+      body: JSON.stringify({
+        className: e.target.name.value,
+        userIds
+      }),
+      headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${authToken}`
+    }
+    })
+  }
+  render(){
     return (
-        <h2>TeacherAssignmentForm</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            ClassName:
+            <input type="text" name="name" />
+          </label>
+          <label>
+            Student Usernames:
+            <textarea type="textarea" name="usernames" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
     )
-} 
+  }
+}
--- src/components/views/teacher-form.js
+++ src/components/views/teacher-form.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import TeacherAssignmentForm from './teacher-assignment-form';
 
 export default function TeacherForm(props) {
     return (
         <div className="teacher-form-container">
             <h2>TeacherForm</h2>
-            <h3>{props.view}</h3>
+            <TeacherAssignmentForm />
         </div>
     )
 }
