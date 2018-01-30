a12643cae6e0ebe78d6954272c5cc88d35330fb2
Andy Gaines <andyagaines@gmail.com>
2018-01-30 11:52:40 -0600

    Creates algorithm to calculate grade averages for each student

--- src/components/views/teacher-students.js
+++ src/components/views/teacher-students.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import TeacherStudentsBar from './teacher-students-bar';
 import { connect } from 'react-redux';
 
 export class TeacherStudents extends React.Component {
+    createClassesObjects = () => {
+        let classes = this.props.user.classes.slice();
+
+        for (let i = 0; i < classes.length; i++) {
+            classes[i].points = 0;
+            classes[i].relevantAssignments = this.props.userdata.all.filter(each => each.className === classes[i].className);
+            for (let j = 0; j < classes[i].relevantAssignments.length; j++) {
+                classes[i].points += classes[i].relevantAssignments[j].points;
+            }
+        }
+
+        if (this.props.class !== 'all') {
+            let classObject = classes.filter(each => each.className === this.props.class)[0]
+            classObject.studentGrades = {}
+            for (let i = 0; i < classObject.studentIds.length; i++) {
+                classObject.studentGrades[[classObject.studentIds[i]]] = 0
+                for (let j = 0; j < classObject.relevantAssignments.length; j++) {
+                    let relevantStudent = classObject.relevantAssignments[j].students.filter(each => each.username === classObject.studentIds[i])[0];
+                    
+                    if (relevantStudent.grade) {
+                        classObject.studentGrades[[classObject.studentIds[i]]] += relevantStudent.pointsEarned
+                    }
+                }
+            }
+            return <TeacherStudentsBar points={classObject.points} studentIds={classObject.studentIds} studentGrades={classObject.studentGrades} />
+        }
+    }
+
     render() {
-        console.log(this.props);
         return (
-            <h2>Teacher Students</h2>
+            <div className="teacher-students-container">
+                <h2>Teacher Students</h2>
+                {this.createClassesObjects()}
+            </div>
         )
     }
 }
