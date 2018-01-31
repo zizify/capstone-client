c7898330080bf8368834431049e84f2be4126f46
Onesun15 <sunnysharma15@hotmail.com>
2018-01-31 17:34:53 -0500

    working on comments

--- src/components/views/teacher-grade-bar.js
+++ src/components/views/teacher-grade-bar.js
@@ -4,28 +4,29 @@ import Collapsible from 'react-collapsible';
 export default function TeacherGradeBar(props) {
   const onSubmit = e => {
     e.preventDefault();
+    console.log('1', e.target.value)
     e.target.teacherComment.value = '';
+    console.log('2', e.target.value)
   };
   console.log('teacherGradesBar', props.students);
   const studentGradesBar = props.students.map((each, index) => {
     return (
       <Collapsible
-        trigger={`${each.username}${each.grade}%${each.pointsEarned}`}
+        trigger={`${each.username} ${each.grade}% ${each.pointsEarned}`}
         key={index}
       >
         <input type="number" id="points" size="1" />
         <label>
-          {studentGradesBar}
-          <textarea type="textarea" id="teacherComment" name="teacherComment"  onChange={e => console.log(e.target.value)}/>
+          <textarea
+            type="textarea"
+            id="teacherComment"
+            name="sss"
+            // onChange={e => console.log(e.target.value)}
+          />
         </label>
         <input type="submit" value="Submit" />
       </Collapsible>
     );
   });
-  return (
-    <form
-    onSubmit={onSubmit}>
-      {studentGradesBar}
-    </form>
-  );
+  return <form onSubmit={e => onSubmit(e)}>{studentGradesBar}</form>;
 }
