9bff836dd51b2f77ec853ba2b669499bc934dd26
Andy Gaines <andyagaines@gmail.com>
2018-02-02 15:13:32 -0600

    Fixes long percentages in teacher students

--- src/components/views/teacher-students-bar.js
+++ src/components/views/teacher-students-bar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export default function TeacherStudentsBar(props) {
+    console.log(props)
     const studentBars = props.studentIds.map((each, index) => {
         return (
             <li key={index}>
                 <div className="teacher-student-bar">
                     <h3 className="teacher-student-bar-username">{each}</h3>
                     <h3 className="teacher-student-bar-points">{props.studentGrades[each]}/{props.points}</h3>
-                    <h3 className="teacher-student-bar-average">{props.points ? ((props.studentGrades[each]/props.points)*100) : '--'}%</h3>
+                    <h3 className="teacher-student-bar-average">{props.points ? Math.floor(((props.studentGrades[each]/props.points)*100)) : '--'}%</h3>
                 </div>
             </li>
         )
